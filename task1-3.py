df8d198ef8b50265a030df9966ed9ac69fec0654
uczlj91 <34397646+uczlj91@users.noreply.github.com>
2020-01-11 14:41:03 +0000

    read itn, build rtree

--- task1-3.py
+++ task1-3.py
@@ -34,13 +34,33 @@ class FloodEmergencyModel(object):
     def bound_polygon(self, r):
         return Polygon([(r.left, r.top), (r.right, r.top), (r.right, r.bottom), (r.left, r.bottom)])
 
+    def read_geojson(self, filepath):
+        with codecs.open(filepath) as fin:
+            return json.load(fin)
+
     def load(self):
         # load elevation data
-        self.elevation = rasterio.open("Material/elevation/SZ.asc")
+        self.elevation = rasterio.open("../Material/elevation/SZ.asc")
         self.elevation_band = self.elevation.read(1)
 
-        # read elevation array
-        elevation_matrix = elevation.read(1)
+        # for task 1, restrict user point in a range that is more than 5km from the bounding edge
+        self.bound_polygon = self.bound_polygon(self.elevation.bounds)
+        self.inner_polygon = self.bound_polygon.buffer(-5000)
+
+        # load itn data
+        self.itndata = self.read_geojson("../Material/itn/solent_itn.json")
+        self.road_nodes = self.itndata["roadnodes"]
+
+        # for task 3, build rtree from itn data
+        count = 0
+        self.itn_rtree = index.Index()
+        for fid, node in self.road_nodes.items():
+            corrds = node["coords"].copy()
+            self.itn_rtree.insert(count, corrds, fid)
+            count+=1
+
+
+
 
     # define a function to get elevation value of user input location
     def get_elevation(self, x, y):
